0a518fd37dd367786180757c6840778b22489229
Sofia Constenla <sofi.amc1995@gmail.com>
2021-11-13 18:30:14 -0300

    commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,30 +1,42 @@
 import { useCartContext } from '../Context/CartContext'
 import {Link} from 'react-router-dom'
+import {useState, useEffect} from 'react'
 
 
 const Cart = () => {
-    
-    const { cartList } = useCartContext()
+
+    const { cartList, isInCart } = useCartContext();
+
+
+
 
     return (
         <>
-            {cartList.length > 0 ? //HACER UNA CONDICION DENTRO DE LA CONDICION?? Si el item.id es != a los item.id del cartList!!!
-                cartList.map((item) =>  //hay que cambiar la condicion y que solo renderice si hay algo nuevo.  
-                    <div>
-                        <h5>Nombre: {item.name}</h5>
-                        <span>Subtotal: {item.subtotal}</span>
-                    </div>
-                
-            ):
+
+        {
+            cartList.length === 0 && 
             <div className="w-100 m-auto">
             <h1>El carrito está vacío</h1>
             <Link to="/">
             <button className="btn btn-primary">Volver al home</button>
             </Link>
             </div>
-            }
+
+        }
+            {cartList.length > 0 && cartList.find(it=> isInCart(it.id)) ?
+        
+                cartList.map((item) =>  (
+                        <ul key={item.id}>
+                            <li>Nombre: {item.name}</li>
+                            <li>Subtotal: {item.subtotal}</li>
+                        </ul>
+                    
+                )                    
+                
+            ):
+            <h1>PRUEBA</h1>}
         </>
     )
 }
 
-export default  Cart;
+export default Cart;
